e1bd24dcb5d339b7d3c2cbecc6c3e85ac9cd0784
Renan de Almeida <89488800+tolentinorenan@users.noreply.github.com>
2021-11-17 10:57:05 -0300

    flappybird
    class desenhar

--- flappybird.py
+++ flappybird.py
@@ -32,7 +32,7 @@ class Passaro:
         self.altura = self.y
         self.tempo = 0
         self.contagem_imagem = 0
-        self.imagem = IMGS[0]
+        self.imagem = self.IMGS[0]
 
     def pular(self):
         self.velocidade = -10.5
@@ -60,6 +60,33 @@ class Passaro:
             if self.angulo > -90:
                 self.angulo -= self.VELOCIDADE_ROTACAO
 
+    def desenhar(self, tela):
+        # definir qual imagem usar
+        self.contagem_imagem += 1
+
+        if self.contagem_imagem < self.TEMPO_DE_ANIMACAO:
+            self.imagem = self.IMGS[0]
+        elif self.contagem_imagem < self.TEMPO_DE_ANIMACAO * 2:
+            self.imagem = self.IMGS[1]
+        elif self.contagem_imagem < self.TEMPO_DE_ANIMACAO * 3:
+            self.imagem = self.IMGS[2]
+        elif self.contagem_imagem < self.TEMPO_DE_ANIMACAO * 4:
+            self.imagem = self.IMGS[1]
+        elif self.contagem_imagem >= self.TEMPO_DE_ANIMACAO * 4 + 1:
+            self.imagem = self.IMGS[0]
+
+
+        # se o passaro cair, não bater asa
+        if self.angulo <= -80:
+            self.imagem = self.IMGS[1]
+            self.contagem_imagem = self.TEMPO_DE_ANIMACAO * 2
+
+        # desenhar imagem
+
+        imagem_rotacionada = pygame.transform.rotate(self.imagem, self.angulo)
+        posicao_centro_imagem = self.imagem.get_rect(topleft = (self.x, self.y)).center
+        retangulo = imagem_rotacionada.get_rect(center = posicao_centro_imagem )
+        tela.blit(imagem_rotacionada, retangulo.topleft)
 
 class Cano:
     pass
